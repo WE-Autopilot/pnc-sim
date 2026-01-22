4379ea5605a96ce5b25518851068b5ef64381112
Aly Ashour <alyashour1@gmail.com>
2026-01-22 17:14:18 -0500

    New message types (partial)

---
+++ include/ap1/pnc_sim/sim.hpp
@@ -0,0 +1,22 @@
+/**
+ * Created: Jan 22, 2026
+ * Author(s): Aly Ashour
+ */
+
+#ifndef AP1_SIM_HPP
+#define AP1_SIM_HPP
+
+#include <vector>
+#include "ap1/pnc_sim/entity.hpp"
+
+namespace ap1::sim {
+    class Sim {
+    public:
+        Sim() {}
+        void update(const double DT) {}
+    private:
+        std::vector<Entity> entities;
+    };
+} // namespace ap1::sim
+
+#endif // AP1_SIM_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,38 @@
+#include <chrono>
+#include <thread>
 #include "ap1/pnc_sim/sim_node.hpp"
+#include "ap1/pnc_sim/sim.hpp"
+
+#define UPDATE_RATE 100 // hz
+
+constexpr auto SIM_DT = std::chrono::duration<double>(1.0 / UPDATE_RATE);
 
 int main(int argc, char ** argv)
 {
+    // make sim
+    auto sim = ap1::sim::Sim();
+
+    // make ros node
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<ap1::sim::SimpleVehicleSimNode>();
-    rclcpp::spin(node);
+    auto ros_node = std::make_shared<ap1::sim::SimpleVehicleSimNode>();
+
+    // spin ros node in separate thread
+    rclcpp::executors::SingleThreadedExecutor exec;
+    exec.add_node(ros_node);
+
+    // update the sim at UPDATE_RATE updates per second
+    bool running = true;
+    while (running && rclcpp::ok()) {
+        auto frame_start = std::chrono::steady_clock::now();
+
+        // update
+        sim.update(SIM_DT.count());     // physics
+        ros_node->publish(sim);         // publish state
+
+        std::this_thread::sleep_until(frame_start + SIM_DT);
+    }
+
     rclcpp::shutdown();
+
     return 0;
 }
